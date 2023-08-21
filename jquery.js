fceca61061cd575119565deac12bb12f8276d517
Vishal Kumar <67638997+vishal8218@users.noreply.github.com>
2023-08-21 13:41:23 +0530

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,10 +1,1 @@
-$("p").on({
-    mouseenter: function(){
-        $(this).css("color", "black");
-    }
-});   
-$("p").on({
-    mouseleave: function(){
-        $(this).css("color","gray");
-    }
-});    
+
